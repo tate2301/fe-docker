583e20005ef0bba7c0b115a6d786b60cba3943f9
nanbratan <morfemi9498@gmail.com>
2020-09-22 20:02:30 +0300

    SelectedFilter component

---
+++ react/src/js/components/__tests__/constants/selectedFilter.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const DEFAULT_PROPS = {
+    id: 'filter id',
+    name: 'filter name',
+    parentId: 'parent id',
+
+    onClick: jest.fn(),
+};
