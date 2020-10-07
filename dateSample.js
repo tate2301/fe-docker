2b2675b2125a685b1c93e60fea4869ac599f8204
someAuthorName <sanjayrai@Sanjays-MacBook-Pro.local>
2020-10-06 22:17:48 -0700

    Updating dateSample.js

--- dateSample.js
+++ dateSample.js
@@ -2,12 +2,6 @@ const luxon = require('luxon');
 
 const { DateTime, Interval } = luxon;
 
-/**
- * E.g. Oct 20 | 13:00 - 14:45 PDT
- * @type {string}
- */
-const i18nFormat = '{date} | {timeRange} {timeZone}';
-
 function getLocalTimeZone(someTimeUTC) {
     const someTime = DateTime.fromISO(someTimeUTC);
     return someTime.toFormat('ZZZZ');
@@ -25,11 +19,16 @@ function getDate(startTimeUTC, someLocale) {
     return start.toFormat('LLL dd', { locale: someLocale });
 }
 
-function prettyFormatDate(locale, startDateUTC, endDateUTC) {
+/**
+ * E.g. Oct 20 | 13:00 - 14:45 PDT
+ * @type {string}
+ */
+function prettyFormatDate(startDateUTC, endDateUTC, locale, i18nFormat) {
     return i18nFormat
         .replace('{date}', getDate(startDateUTC, locale))
         .replace('{timeRange}', getTimeInterval(startDateUTC, endDateUTC, locale))
         .replace('{timeZone}', getLocalTimeZone(endDateUTC));
 }
 
-console.log(prettyFormatDate('de', '2020-10-20T20:00:00Z', '2020-10-20T21:45:00Z'));
+const prettyFormat = prettyFormatDate('2020-10-20T20:00:00Z', '2020-10-20T21:45:00Z', 'de', '{date} | {timeRange} {timeZone}');
+console.log(prettyFormat);
