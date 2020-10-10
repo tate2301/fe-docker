b6d990ae9bff27e8d689faae3279f6de95b6334c
Davin Shaun <shaundavin.se@gmail.com>
2020-10-10 18:38:50 +0700

    Improve readability in Bookmarks.jsx

--- react/src/js/components/Consonant/Bookmarks/Bookmarks.jsx
+++ react/src/js/components/Consonant/Bookmarks/Bookmarks.jsx
@@ -14,30 +14,24 @@ const Bookmarks = (props) => {
     const selectedIco = getConfig('bookmarks', 'selectBookmarksIcon');
     const unselectedIco = getConfig('bookmarks', 'unselectBookmarksIcon');
 
-    const renderIcon = () => {
-        let src = '';
-
-        if (selected && selectedIco) src = selectedIco;
-        if (!selected && unselectedIco) src = unselectedIco;
-
-        return src ?
-            <span
-                data-testid="bookmarks--ico"
-                className="bookmarks--ico"
-                style={{ backgroundImage: `url(${src})` }} /> :
-            <span data-testid="bookmarks--ico" className="bookmarks--ico" />;
-    };
+    const src = selected ? selectedIco : unselectedIco;
 
     return (
         <button
             data-testid="bookmarks"
             type="button"
             onClick={onClick}
-            className={selected ? 'bookmarks bookmarks_selected' : 'bookmarks'
-            }
+            className={selected ? 'bookmarks bookmarks_selected' : 'bookmarks'}
             tabIndex="0">
             <span className="bookmarks--ico-wrapper">
-                {renderIcon()}
+                {src ? (
+                    <span
+                        data-testid="bookmarks--ico"
+                        className="bookmarks--ico"
+                        style={{ backgroundImage: `url(${src})` }} />
+                ) : (
+                    <span data-testid="bookmarks--ico" className="bookmarks--ico" />
+                )}
                 <span className="bookmarks--title">{title}</span>
             </span>
             <span data-testid="bookmarks--item-badge" className="bookmarks--item-badge">{qty}</span>
