c82dfa430f5b16d8ade04c8d7e9b060219a8db98
Davin Shaun <shaundavin.se@gmail.com>
2020-10-10 18:53:05 +0700

    Improve readability of LoadMore.jsx

--- react/src/js/components/Consonant/Pagination/LoadMore.jsx
+++ react/src/js/components/Consonant/Pagination/LoadMore.jsx
@@ -2,16 +2,16 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { useConfig } from '../../../utils/hooks';
 
-const LoadMore = (props) => {
-    const {
-        onClick, show, total,
-    } = props;
-
+const LoadMore = ({
+    onClick,
+    show,
+    total,
+}) => {
     const getConfig = useConfig();
     const loadMoreButtonText = getConfig('pagination', 'loadMoreButtonText');
     const loadMoreQuantityText = getConfig('pagination', 'loadMoreQuantityText');
 
-    const renderQtyHTML = () => loadMoreQuantityText
+    const qtyHTML = loadMoreQuantityText
         .replace('{}', `<span class="consonant-load-more--shown">${show}</span>`)
         .replace('{}', `<span class="consonant-load-more--total">${total}</span>`);
 
@@ -22,7 +22,7 @@ const LoadMore = (props) => {
                 <p
                     data-testid="consonant-load-more--text"
                     className="consonant-load-more--text"
-                    dangerouslySetInnerHTML={{ __html: renderQtyHTML() }} />
+                    dangerouslySetInnerHTML={{ __html: qtyHTML }} />
                 {show < total &&
                     <button
                         type="button"
