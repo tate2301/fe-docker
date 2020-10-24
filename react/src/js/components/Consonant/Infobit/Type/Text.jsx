8fc1563563e8924b0ab741a67b5f0ead6af98862
someAuthorName <sanjayrai@Sanjays-MacBook-Pro.local>
2020-10-23 23:41:44 -0700

    Refactor of Infobit components

--- react/src/js/components/Consonant/Infobit/Type/Text.jsx
+++ react/src/js/components/Consonant/Infobit/Type/Text.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Text({ text }) {
-    return <p className="consonant-text-infobit">{text}</p>;
-}
+/**
+ * Text infobit (shown in 3:2 Card Footer)
+ *
+ * @component
+ * @example
+ * const props= {
+    text: String,
+ * }
+ * return (
+ *   <Text {...props}/>
+ * )
+ */
+const Text = ({ text }) => <p className="consonant-text-infobit">{text}</p>;
 
 Text.propTypes = {
     text: PropTypes.string.isRequired,
