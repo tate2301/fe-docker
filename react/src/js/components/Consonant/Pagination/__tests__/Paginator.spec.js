34f59fd5c3537e93111d409b03b6eed95285243e
someAuthorName <sanjayrai@Sanjays-MacBook-Pro.local>
2020-10-23 21:34:29 -0700

    Paginator test refactor

---
+++ react/src/js/components/Consonant/Pagination/__tests__/Paginator.spec.js
@@ -0,0 +1,90 @@
+import '@testing-library/jest-dom/extend-expect';
+import {
+    screen,
+    fireEvent,
+    getNodeText,
+} from '@testing-library/react';
+
+import { getItemsRange } from '../../Helpers/Testing/Utils/Pagination';
+import config from '../../Helpers/Testing/Mocks/config.json';
+import Paginator from '../Paginator';
+import setup from '../../Helpers/Testing/Utils/Settings';
+
+import {
+    PAGE_LIST,
+    DEFAULT_PROPS,
+} from '../../Helpers/Testing/Constants/Pagination';
+
+const renderComponent = setup(Paginator, DEFAULT_PROPS);
+
+const { pagination: { i18n: { paginator: { resultsQuantityText } } } } = config;
+
+describe('Consonant/Pagination', () => {
+    test('Pagination summary should work for different page ranges', () => {
+        PAGE_LIST.forEach((page) => {
+            const { props: { showItemsPerPage, totalResults }, wrapper } =
+                renderComponent({ currentPageNumber: page });
+
+            const { start, end } = getItemsRange({
+                page, totalResults, itemsPerPage: showItemsPerPage,
+            });
+
+            const itemsToDisplay = {
+                start, end, total: totalResults,
+            };
+
+            const summary = resultsQuantityText
+                .replace(/\{(\w*)}/gi, (_, matchedKey) => itemsToDisplay[matchedKey]);
+
+            const paginationSummaryElement = screen.getByTestId('pagination--summary');
+            expect(paginationSummaryElement).toHaveTextContent(summary);
+
+            wrapper.unmount();
+        });
+    });
+    test('Should be able to go navigate and forward', () => {
+        const { props: { onClick } } = renderComponent();
+
+        const prevButton = screen.getByTestId('btn_prev');
+        const nextButton = screen.getByTestId('btn_next');
+        const pageButton = screen.queryAllByTestId('btn_page')[0];
+
+        fireEvent.click(nextButton);
+        expect(onClick).toHaveBeenCalledWith(2);
+
+        fireEvent.click(prevButton);
+        expect(onClick).toHaveBeenCalledWith(1);
+
+        fireEvent.click(pageButton);
+        const buttonText = getNodeText(pageButton);
+        expect(onClick).toHaveBeenCalledWith(Number(buttonText));
+    });
+    test('Should be able to handle invalid current page and reset back to a valid page', () => {
+        const { props: { onClick } } = renderComponent({ currentPageNumber: 0 });
+        const prevButton = screen.getByTestId('btn_prev');
+
+        fireEvent.click(prevButton);
+        expect(onClick).toHaveBeenCalledWith(1);
+    });
+    test('Should be able to go one page forward', () => {
+        const { props: { onClick } } = renderComponent({ currentPageNumber: 2, totalPages: 1 });
+        const nextButton = screen.getByTestId('btn_next');
+
+        fireEvent.click(nextButton);
+        expect(onClick).toHaveBeenCalledWith(1);
+    });
+    test('Should be able to go back a page', () => {
+        const { props: { onClick } } = renderComponent({ currentPageNumber: 5 });
+        const prevButton = screen.getByTestId('btn_prev');
+
+        fireEvent.click(prevButton);
+        expect(onClick).toHaveBeenCalledWith(4);
+    });
+    test('If on page one and previous is clicked, we should not go to page 0', () => {
+        const { props: { onClick } } = renderComponent({ currentPageNumber: 1 });
+        const prevButton = screen.getByTestId('btn_prev');
+
+        fireEvent.click(prevButton);
+        expect(onClick).toHaveBeenCalledWith(1);
+    });
+});
