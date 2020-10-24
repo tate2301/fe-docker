5300450131d60c9a7fa5fc615eacafdee8fdc9ec
someAuthorName <sanjayrai@Sanjays-MacBook-Pro.local>
2020-10-24 15:57:31 -0700

    Refactor of Button.jsx

--- react/src/js/components/Consonant/Infobit/Type/Button.jsx
+++ react/src/js/components/Consonant/Infobit/Type/Button.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import className from 'classnames';
 
 const BUTTON_STYLE = {
     CTA: 'call-to-action',
     PRIMARY: 'primary',
 };
 
-function Button({ style, text, href }) {
+/**
+ * Button Infobit (shown in 3:2 Card Footer)
+ *
+ * @component
+ * @example
+ * const props= {
+    style: String,
+    href: String,
+    text: String,
+ * }
+ * return (
+ *   <Button {...props}/>
+ * )
+ */
+const Button = ({
+    style,
+    text,
+    href,
+}) => {
+    const isCtaButton = style === BUTTON_STYLE.CTA;
+    const buttonClass = className({
+        'consonant-btn-infobit': true,
+        'consonant-btn-infobit_cta': isCtaButton,
+    });
     return (
         <a
-            className={style === BUTTON_STYLE.CTA ?
-                'consonant-btn-infobit consonant-btn-infobit_cta' :
-                'consonant-btn-infobit'}
+            className={buttonClass}
             data-testid="consonant-btn-infobit"
             tabIndex="0"
             rel="noreferrer"
             target="_blank"
-            href={href}
-        >
+            href={href}>
             <span>{text}</span>
         </a>
     );
-}
+};
 
 Button.propTypes = {
     style: PropTypes.string,
