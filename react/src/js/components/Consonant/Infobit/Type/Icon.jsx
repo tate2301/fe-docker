0956c2247a22c650c7e711ea80953644be8523bb
someAuthorName <sanjayrai@Sanjays-MacBook-Pro.local>
2020-10-23 23:59:21 -0700

    Refactor of Icon w/ Text Infobit

--- react/src/js/components/Consonant/Infobit/Type/Icon.jsx
+++ react/src/js/components/Consonant/Infobit/Type/Icon.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Icon({ src, alt }) {
-    return (<img
+/**
+ * Icon With Text Infobit (shown in 3:2 Card Footer)
+ *
+ * @component
+ * @example
+ * const props= {
+    src: String,
+    srcAltText: String,
+    text: String,
+ * }
+ * return (
+ *   <IconWithText {...props}/>
+ * )
+ */
+const Icon = ({
+    src,
+    alt,
+}) => (
+    <img
         className="consonant-icon-infobit"
         width="28"
         src={src}
         alt={alt}
         loading="lazy" />);
-}
 
 Icon.propTypes = {
     src: PropTypes.string.isRequired,
