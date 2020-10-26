fdc4cad505cf54ad0f764f0212252463c0c050eb
someAuthorName <sanjayrai@Sanjays-MacBook-Pro.local>
2020-10-25 19:05:15 -0700

    Finishing up test refactor for left filter panel

--- react/src/js/components/Consonant/Filters/Left/__tests__/Panel.spec.js
+++ react/src/js/components/Consonant/Filters/Left/__tests__/Panel.spec.js
@@ -29,11 +29,10 @@ describe('Consonant/Left/Panel', () => {
         });
 
         const footerTotalResElement = screen.queryByTestId('mobile-footer-total-res');
-
         expect(footerTotalResElement).not.toBeNull();
     });
 
-    test('should be able too render mobile/tablet elements when filters selected', () => {
+    test('should be able to render mobile/tablet elements when filters selected', () => {
         const {
             config: {
                 filterPanel: { i18n: { leftPanel: { mobile: { panel: { applyBtnText } } } } },
@@ -44,11 +43,9 @@ describe('Consonant/Left/Panel', () => {
         });
 
         const mobileFooterClearElement = screen.queryByTestId('mobile-footer-clear');
-
         expect(mobileFooterClearElement).not.toBeNull();
 
         const mobileFooterBtnElement = screen.queryByTestId('mobile-footer-btn');
-
         expect(mobileFooterBtnElement).toHaveTextContent(applyBtnText);
     });
 
@@ -56,23 +53,20 @@ describe('Consonant/Left/Panel', () => {
         const { props: { onFilterClick } } = renderFilterPanel();
 
         const [filterItemElement] = screen.queryAllByTestId('filter-item-link');
-
         expect(filterItemElement).toBeDefined();
 
         fireEvent.click(filterItemElement);
-
         expect(onFilterClick).toBeCalled();
     });
     test('Should be able to clear all filters', () => {
         const { props: { onClearAllFilters } } = renderFilterPanel();
 
         const clearButtonElement = screen.queryByTestId('left-filter-panel-clear-all-btn');
-
         expect(clearButtonElement).not.toBeNull();
 
         fireEvent.click(clearButtonElement);
-
         expect(onClearAllFilters).toBeCalled();
+
         onClearAllFilters.mockClear();
     });
     test('Should be able to clear all filters using a mobile footer', () => {
@@ -82,12 +76,11 @@ describe('Consonant/Left/Panel', () => {
         });
 
         const mobileFooterClearElement = screen.queryByTestId('mobile-footer-clear');
-
         expect(mobileFooterClearElement).not.toBeNull();
 
         fireEvent.click(mobileFooterClearElement);
-
         expect(onClearAllFilters).toBeCalled();
+        
         onClearAllFilters.mockClear();
     });
     test('should be able to toggle on mobile', () => {
@@ -96,19 +89,15 @@ describe('Consonant/Left/Panel', () => {
         });
 
         const mobileButtonBackElement = screen.queryByText('Back');
-
         expect(mobileButtonBackElement).not.toBeNull();
 
         fireEvent.click(mobileButtonBackElement);
-
         expect(onMobileFiltersToggleClick).toHaveBeenCalledTimes(1);
 
         const mobileFooterBtnElement = screen.queryByTestId('mobile-footer-btn');
-
         expect(mobileFooterBtnElement).not.toBeNull();
 
         fireEvent.click(mobileFooterBtnElement);
-
         expect(onMobileFiltersToggleClick).toHaveBeenCalledTimes(2);
     });
 });
--- react/src/js/components/Consonant/Filters/Left/__tests__/SelectedItem.spec.js
+++ react/src/js/components/Consonant/Filters/Left/__tests__/SelectedItem.spec.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import {
+    render,
+    screen,
+} from '@testing-library/react';
 
 import { SelectedItem } from '../Desktop-Only/SelectedItem';
 
-
 describe('Consonant/Left/Selected Filters', () => {
-    test('If no filters are selected, filter count should not appear ', async () => {
+    test('If no filters are selected, a filter count should not appear ', async () => {
         const props = {
             numItemsSelected: 0,
             handleClear: () => {},
