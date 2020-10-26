bc9dcde54ed4d19de91546ac65f0ad0cbc861898
someAuthorName <sanjayrai@Sanjays-MacBook-Pro.local>
2020-10-25 19:11:26 -0700

    Refactor of top item tests

--- react/src/js/components/Consonant/Filters/Top/__tests__/Item.spec.js
+++ react/src/js/components/Consonant/Filters/Top/__tests__/Item.spec.js
@@ -1,76 +1,60 @@
-import { screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import {
+    screen,
+    fireEvent,
+} from '@testing-library/react';
 
 import { Group as Item } from '../Group';
-
+import setup from '../../../Testing/Utils/Settings';
 import {
     DEFAULT_PROPS,
     selectedAllItems,
 } from '../../../Testing/Constants/FilterItem';
 
-import makeSetup from '../../../Testing/Utils/Settings';
-
-const setup = makeSetup(Item, DEFAULT_PROPS);
+const renderTopFilterItem = setup(Item, DEFAULT_PROPS);
 
 describe('Consonant/Filters/Top/Item', () => {
-    test('should render without item count badge', () => {
-        setup();
+    test('Should be able to render without an item count badge', () => {
+        renderTopFilterItem();
 
         const badgeElement = screen.queryByTestId('item-badge');
-
         expect(badgeElement).toBeNull();
     });
-    test('should render all list items', () => {
-        const { props: { items } } = setup();
+
+    test('Should be able to render all list items', () => {
+        const { props: { items } } = renderTopFilterItem();
 
         const filterItemElement = screen.queryAllByTestId('filter-group-item');
-
         expect(filterItemElement).toHaveLength(items.length);
     });
-    test('shouldn`t cliped items if items length < 9', () => {
+
+    test('Items should NOT be clipped if item length is less than 9', () => {
         const defaultItems = [...DEFAULT_PROPS.items];
         defaultItems.length = 5;
 
-        setup({ items: defaultItems });
+        renderTopFilterItem({ items: defaultItems });
 
         const filterGroupElement = screen.getByTestId('filter-group');
-
         expect(filterGroupElement).not.toHaveClass('consonant-top-filter--items_clipped');
     });
 
-    describe('Interaction with UI', () => {
-        test('should call onCheck', () => {
-            const { props: { onCheck } } = setup();
+    test('Clicking filter checkboxes should work', () => {
+        const { props: { onCheck } } = renderTopFilterItem();
 
-            const [checkboxElement] = screen.queryAllByTestId('list-item-checkbox');
+        const [checkboxElement] = screen.queryAllByTestId('list-item-checkbox');
+        expect(checkboxElement).toBeDefined();
 
-            expect(checkboxElement).toBeDefined();
+        fireEvent.click(checkboxElement);
+        expect(onCheck).toBeCalled();
+    });
 
-            fireEvent.click(checkboxElement);
+    test('Clicking the clear all button should work', () => {
+        const { props: { onClearAll } } = renderTopFilterItem(selectedAllItems);
 
-            expect(onCheck).toBeCalled();
-        });
-        // test('should call onClick', () => {
-        //     const { props: { onClick, name } } = setup();
-        //
-        //     const itemLinkElement = screen.getByText(name);
-        //
-        //     expect(itemLinkElement).not.toBeNull();
-        //
-        //     fireEvent.click(itemLinkElement);
-        //
-        //     expect(onClick).toBeCalled();
-        // });
-        test('should call onClearAll', () => {
-            const { props: { onClearAll } } = setup(selectedAllItems);
+        const clearBtn = screen.queryByTestId('clear-btn');
+        expect(clearBtn).not.toBeNull();
 
-            const clearBtn = screen.queryByTestId('clear-btn');
-
-            expect(clearBtn).not.toBeNull();
-
-            fireEvent.click(clearBtn);
-
-            expect(onClearAll).toBeCalled();
-        });
+        fireEvent.click(clearBtn);
+        expect(onClearAll).toBeCalled();
     });
 });
