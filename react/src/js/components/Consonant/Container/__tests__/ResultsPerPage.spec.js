5eb4b2a733213893b563c0570adfa4a805191277
someAuthorName <sanjayrai@Sanjays-MacBook-Pro.local>
2020-10-25 17:03:31 -0700

    Refactor of resultsPerPage tests

--- react/src/js/components/Consonant/Container/__tests__/ResultsPerPage.spec.js
+++ react/src/js/components/Consonant/Container/__tests__/ResultsPerPage.spec.js
@@ -1,96 +1,56 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import {
     screen,
     waitFor,
-    fireEvent,
-    getByText,
-    getByTestId,
-    queryAllByTestId,
     act,
-    logDOM,
     render,
 } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
 
 import Container from '../Container';
-
+import setupIntersectionObserverMock from '../../Testing/Mocks/intersectionObserver';
 import config from '../../Testing/Mocks/config.json';
 import cards from '../../Testing/Mocks/cards.json';
-
-import makeInit from '../../Testing/Utils/Init';
-import React from 'react';
-
-// Different window sizes for different cases
-const MOBILE_WIDTH = 384;
-const DESKTOP_WIDTH = 1800;
-const TABLET_MIN_WIDTH = 768;
-const DESKTOP_MIN_WIDTH = 1200;
-
-const init = makeInit(Container, config);
-
-const { filterPanel: { filters }, collection: { endpoint } } = config;
-
-const filteredCards = cards.filter(({ appliesTo }) => Boolean(appliesTo));
-
-// Mock api to get card list
-const handlers = [
-    rest.get(endpoint, (req, res, ctx) => res(
-        ctx.status(200),
-        ctx.json({ cards }),
-    )),
-];
 
 global.fetch = jest.fn(() =>
     Promise.resolve({
         json: () => Promise.resolve({ cards }),
     }));
 
-window.scrollTo = () => { };
-jest.setTimeout(30000);
+setupIntersectionObserverMock();
 
-describe('Testing Results Per Page', () => {
-
-    test('should render limited count of card', async () => {
-
+describe('Consonant/Results Per Page', () => {
+    test('should render default count of cards if incorrectly authored', async () => {
         const configToUse = config;
         configToUse.collection.resultsPerPage = null;
-        await act(async () => render(<Container config={configToUse} />));
 
-        // Need wait for api response and state updating
+        await act(async () => render(<Container config={configToUse} />));
         await waitFor(() => screen.getByTestId('consonant-collection'));
 
-        /**
-         * If resultsPerPage didn't present - we should render all cards
-         */
         expect(screen.queryAllByTestId('consonant-card-3-2')).toHaveLength(9);
     });
 
-    test('should render 1 cards by default if resultsPerPage === 1', async () => {
+    test('should render 1 card if authored to do so', async () => {
         const configToUse = config;
         configToUse.collection.resultsPerPage = 1;
-        await act(async () => render(<Container config={configToUse} />));
 
-        // Need wait for api response and state updating
+        await act(async () => render(<Container config={configToUse} />));
         await waitFor(() => screen.getByTestId('consonant-collection'));
 
         expect(screen.queryAllByTestId('consonant-card-3-2')).toHaveLength(1);
     });
 
-    test('should render all cards if limit > cards count', async () => {
+    test('should render all cards if limit > total cards count', async () => {
         const configToUse = config;
         configToUse.collection.resultsPerPage = 100;
-        await act(async () => render(<Container config={configToUse} />));
 
-        // Need wait for api response and state updating
+        await act(async () => render(<Container config={configToUse} />));
         await waitFor(() => screen.getByTestId('consonant-collection'));
 
-        /**
-         * if totalCardLimit > cards.length then we should render all cards
-         */
-        // -2 excludes one 1:1 card and one full-card
-        const totalCardLength = cards.length + config.featuredCards.length - 3;
-        expect(screen.queryAllByTestId('consonant-card-3-2')).toHaveLength(totalCardLength);
+        const allCards = cards.length;
+        const notThreeByTwoCard = cards.filter(card => card.styles.typeOverride === '1:1' || card.styles.typeOverride === 'full-card').length;
+        const allThreeByTwoCards = allCards - notThreeByTwoCard;
+        const featuredCardsLength = config.featuredCards.length;
+        expect(screen.queryAllByTestId('consonant-card-3-2')).toHaveLength(allThreeByTwoCards + featuredCardsLength);
     });
 });
