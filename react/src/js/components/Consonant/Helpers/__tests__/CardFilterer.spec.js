4d62f9e35847433447609eb1537bbb1deabe28ed
nanbratan <morfemi9498@gmail.com>
2020-10-28 20:42:15 +0300

    added tests for utils/CardFilterer.js

---
+++ react/src/js/components/Consonant/Helpers/__tests__/CardFilterer.spec.js
@@ -0,0 +1,109 @@
+import { render, queryByTestId } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import PROPS from './constants/CardFilterer';
+
+import CardFilterer from '../CardFilterer';
+
+describe('utils/CardFilterer', () => {
+    describe('filterCards', () => {
+        PROPS.filterCards.forEach(({
+            cards, activeFilters, filterType, filterTypes, expectedValue,
+        }) => {
+            test('should return filtered cards', () => {
+                const cardFilterer = new CardFilterer(cards);
+
+                const { filteredCards } = cardFilterer.filterCards(
+                    activeFilters,
+                    filterType,
+                    filterTypes,
+                );
+
+                expect(filteredCards).toEqual(expectedValue);
+            });
+        });
+    });
+    describe('sortCards', () => {
+        PROPS.sortCards.forEach(({ cards, sortOption, expectedValue }) => {
+            test('should return sorted cards', () => {
+                const cardFilterer = new CardFilterer(cards);
+
+                const { filteredCards } = cardFilterer.sortCards(sortOption);
+
+                expect(filteredCards).toEqual(expectedValue);
+            });
+        });
+    });
+    describe('keepCardsWithinDateRange', () => {
+        PROPS.keepCardsWithinDateRange.forEach(({ cards, expectedValue }) => {
+            test(`shouldn return ${expectedValue} value`, () => {
+                const cardFilterer = new CardFilterer(cards);
+
+                const { filteredCards } = cardFilterer.keepCardsWithinDateRange();
+
+                expect(filteredCards).toEqual(expectedValue);
+            });
+        });
+    });
+    describe('keepBookmarkedCardsOnly', () => {
+        PROPS.keepBookmarkedCardsOnly.forEach(({
+            cards,
+            onlyShowBookmarks,
+            bookmarkedCardIds,
+            showBookmarks,
+            expectedValue,
+        }) => {
+            test('should return only bookmarked cards', () => {
+                const cardFilterer = new CardFilterer(cards);
+
+                const { filteredCards } = cardFilterer.keepBookmarkedCardsOnly(
+                    onlyShowBookmarks,
+                    bookmarkedCardIds,
+                    showBookmarks,
+                );
+
+                expect(filteredCards).toEqual(expectedValue);
+            });
+        });
+    });
+    describe('truncateList', () => {
+        PROPS.truncateList.forEach(({ cards, totalCardLimit, expectedValue }) => {
+            test('should return trunkated array', () => {
+                const cardFilterer = new CardFilterer(cards);
+
+                const { filteredCards } = cardFilterer.truncateList(totalCardLimit);
+
+                expect(filteredCards).toEqual(expectedValue);
+            });
+        });
+    });
+    describe('searchCards', () => {
+        test('should highlight searched field correctly', () => {
+            const query = 'name';
+            const searchFields = ['title'];
+            const cards = [{ title: 'title name' }, { description: 'description' }, { title: 'some string' }];
+
+            const cardFilterer = new CardFilterer(cards);
+
+            const { filteredCards } = cardFilterer.searchCards(query, searchFields);
+
+            const { container } = render(filteredCards.map(({ title }) => title || null));
+
+            const highlightElement = queryByTestId(container, 'consonant-search-result');
+
+            expect(highlightElement).not.toBeNull();
+            expect(highlightElement).toHaveTextContent('name');
+        });
+        test('shouldn`t highlight searched field when query.length < 3', () => {
+            const query = '12';
+            const searchFields = ['title'];
+            const cards = [{ title: 'title name' }, { description: 'description' }, { title: 'some string' }];
+
+            const cardFilterer = new CardFilterer(cards);
+
+            const { filteredCards } = cardFilterer.searchCards(query, searchFields);
+
+            expect(filteredCards).toEqual([]);
+        });
+    });
+});
