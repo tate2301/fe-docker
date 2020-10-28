8989643fef2d916552be001f81f727660e53842c
someAuthorName <sanjayrai@Sanjays-MacBook-Pro.local>
2020-10-28 12:03:57 -0700

    Updating description of unit test

--- react/src/js/components/Consonant/Helpers/__tests__/contexts.spec.js
+++ react/src/js/components/Consonant/Helpers/__tests__/contexts.spec.js
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { noOp } from '../contexts';
 
 describe('utils/contexts', () => {
-    test('should return true ', () => {
+    test('no op should return undefined', () => {
         const value = noOp();
         expect(value).toBe(undefined);
     });
