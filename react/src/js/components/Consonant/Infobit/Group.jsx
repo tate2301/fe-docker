7d2b129c78efb742b5f3c9ce43b2bf22eda60dde
Denis Radai <yodeco@gmail.com>
2020-10-28 12:12:31 +0200

    Refactor Infobit/Group.jsx

--- react/src/js/components/Consonant/Infobit/Group.jsx
+++ react/src/js/components/Consonant/Infobit/Group.jsx
@@ -1,9 +1,7 @@
-/* eslint-disable react/no-array-index-key */
-import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import uuid from 'react-uuid';
+import React, { Fragment } from 'react';
 
-import { INFOBIT_TYPE } from '../Helpers/constants';
-import { parseToPrimitive } from '../Helpers/general';
 import Bookmark from './Type/Bookmark/Bookmark';
 import Button from './Type/Button';
 import Icon from './Type/Icon';
@@ -16,51 +14,108 @@ import Rating from './Type/Rating';
 import Text from './Type/Text';
 import DateInterval from './Type/DateInterval';
 
+import { INFOBIT_TYPE } from '../Helpers/constants';
+import { parseToPrimitive } from '../Helpers/general';
+
+/**
+ * Group of Infobits (shown in 3:2 Card Footer)
+ *
+ * @component
+ * @example
+ * const props= {
+    renderList: Array,
+ * }
+ * return (
+ *   <Group {...props}/>
+ * )
+ */
 const Group = (props) => {
-    const { renderList } = props;
+    const {
+        renderList,
+    } = props;
 
     return (
         <Fragment>
-            {renderList.map((infobit, index) => {
+            {renderList.map((infobit) => {
                 switch (infobit.type) {
                     case INFOBIT_TYPE.PRICE:
-                        return <Price {...infobit} key={index} />;
+                        return (
+                            <Price
+                                {...infobit}
+                                key={uuid()} />
+                        );
 
                     case INFOBIT_TYPE.BUTTON:
-                        return <Button {...infobit} key={index} />;
+                        return (
+                            <Button
+                                {...infobit}
+                                key={uuid()} />
+                        );
 
                     case INFOBIT_TYPE.ICON_TEXT:
-                        return <IconWithText {...infobit} key={index} />;
+                        return (
+                            <IconWithText
+                                {...infobit}
+                                key={uuid()} />
+                        );
 
                     case INFOBIT_TYPE.LINK_ICON:
-                        return <LinkWithIcon {...infobit} key={index} />;
+                        return (
+                            <LinkWithIcon
+                                {...infobit}
+                                key={uuid()} />
+                        );
 
                     case INFOBIT_TYPE.TEXT:
-                        return <Text {...infobit} key={index} />;
+                        return (
+                            <Text
+                                {...infobit}
+                                key={uuid()} />
+                        );
 
                     case INFOBIT_TYPE.ICON:
-                        return <Icon {...infobit} key={index} />;
+                        return (
+                            <Icon
+                                {...infobit}
+                                key={uuid()} />
+                        );
 
                     case INFOBIT_TYPE.LINK:
-                        return <TextLink {...infobit} key={index} />;
+                        return (
+                            <TextLink
+                                {...infobit}
+                                key={uuid()} />
+                        );
 
                     case INFOBIT_TYPE.PROGRESS:
-                        return <Progress {...infobit} key={index} />;
+                        return (
+                            <Progress
+                                {...infobit}
+                                key={uuid()} />
+                        );
 
                     case INFOBIT_TYPE.RATING:
                         return (
                             <Rating
-                                key={index}
+                                key={uuid()}
                                 label={infobit.label}
                                 totalStars={parseToPrimitive(infobit.totalStars)}
                                 starsFilled={parseToPrimitive(infobit.starsFilled)} />
                         );
 
                     case INFOBIT_TYPE.BOOKMARK:
-                        return <Bookmark {...infobit} key={index} />;
+                        return (
+                            <Bookmark
+                                {...infobit}
+                                key={uuid()} />
+                        );
 
                     case INFOBIT_TYPE.DATE:
-                        return <DateInterval {...infobit} key={index} />;
+                        return (
+                            <DateInterval
+                                {...infobit}
+                                key={uuid()} />
+                        );
 
                     default: return null;
                 }
