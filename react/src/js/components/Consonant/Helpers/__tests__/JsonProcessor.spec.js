d48fbe427e938b4fe3255e3c103844c6f1fb8a18
nanbratan <morfemi9498@gmail.com>
2020-10-28 20:43:01 +0300

    added tests for utils/JsonProcessor.js

---
+++ react/src/js/components/Consonant/Helpers/__tests__/JsonProcessor.spec.js
@@ -0,0 +1,45 @@
+import PROPS from './constants/JsonProcessor';
+
+import JsonProcessor from '../JsonProcessor';
+
+describe('utils/JsonProcessor', () => {
+    describe('addCardMetaData', () => {
+        PROPS.addCardMetaData.forEach(({
+            cards, truncateTextQty, onlyShowBookmarks, bookmarkedCardIds, expectedValue,
+        }) => {
+            test('should add card metadata', () => {
+                const jsonProcessor = new JsonProcessor(cards);
+
+                const { processedCards } = jsonProcessor.addCardMetaData(
+                    truncateTextQty,
+                    onlyShowBookmarks,
+                    bookmarkedCardIds,
+                );
+
+                expect(processedCards).toEqual(expectedValue);
+            });
+        });
+    });
+    describe('addFeaturedCards', () => {
+        PROPS.addFeaturedCards.forEach(({ cards, featuredCards, expectedValue }) => {
+            test('should merge processed cards and featured cards', () => {
+                const jsonProcessor = new JsonProcessor(cards);
+
+                const { processedCards } = jsonProcessor.addFeaturedCards(featuredCards);
+
+                expect(processedCards).toEqual(expectedValue);
+            });
+        });
+    });
+    describe('removeDuplicateCards', () => {
+        PROPS.removeDuplicateCards.forEach(({ cards, expectedValue }) => {
+            test('should return array with unique elements', () => {
+                const jsonProcessor = new JsonProcessor(cards);
+
+                const { processedCards } = jsonProcessor.removeDuplicateCards();
+
+                expect(processedCards).toEqual(expectedValue);
+            });
+        });
+    });
+});
