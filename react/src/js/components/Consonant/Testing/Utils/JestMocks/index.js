5a5db57d8596cb954a8df8960bfcbf57c91d4905
nanbratan <morfemi9498@gmail.com>
2020-10-28 20:40:37 +0300

    added IntersectionObserver mock

---
+++ react/src/js/components/Consonant/Testing/Utils/JestMocks/index.js
@@ -0,0 +1,7 @@
+import mockLocalStorage from './mockLocalStorage';
+import mockIntersectionObserver from './mockIntersectionObserver';
+
+export default {
+    localStorage: mockLocalStorage,
+    intersectionObserver: mockIntersectionObserver,
+};
