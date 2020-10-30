559f9307aa3e40f5886c1dd9268c0a8b4f4b810b
Denis Radai <yodeco@gmail.com>
2020-10-30 12:27:14 +0200

    Helpers/consonant.js refactored

--- react/src/js/components/Consonant/Helpers/consonant.js
+++ react/src/js/components/Consonant/Helpers/consonant.js
@@ -7,11 +7,23 @@ import {
     isNullish,
 } from './general';
 
+/**
+ * Counts the quantity or the selected filters options
+ * @param {Array} filters - filters array
+ * @returns {Number} - the quantity or the selected filters options
+ */
 export const getNumSelectedFilterItems = (filters) => {
     const filterItems = chainFromIterable(filters.map(filter => filter.items));
     return sum(filterItems.map(item => item.selected));
 };
 
+/**
+ * Returns the authored or default configurational value
+ * @param {Object} config - main configurational object
+ * @param {Object} object - object inside the main configurational object
+ * @param {String} key - an object key for which we need a value
+ * @returns {} - authored or default configurational value
+ */
 export const makeConfigGetter = config => (object, key) => {
     const objectPath = key ? `${object}.${key}` : object;
     const defaultValue = get(DEFAULT_CONFIG, objectPath);
@@ -24,6 +36,12 @@ export const makeConfigGetter = config => (object, key) => {
     return value;
 };
 
+/**
+ * Get the default sorting option
+ * @param {Object} config - configurational object
+ * @param {String} query - title of the sorting option
+ * @returns {Object} - object with the default sorting option
+ */
 export function getDefaultSortOption(config, query) {
     const sortOptions = makeConfigGetter(config)('sort', 'options');
     return sortOptions.find(option => option.sort === query) || {
