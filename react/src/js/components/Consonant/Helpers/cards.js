97fcd0b2c349b1db5d619cc9fc4c1d8209dcd523
Denis Radai <yodeco@gmail.com>
2020-10-30 11:58:06 +0200

    Helpers/cards.js refactored

--- react/src/js/components/Consonant/Helpers/cards.js
+++ react/src/js/components/Consonant/Helpers/cards.js
@@ -1,7 +1,20 @@
 import get from 'lodash/get';
 
+/**
+ * Converts date to the number of milliseconds
+ * @param {String} date - date as a string
+ * @returns {Number} - a number representing the milliseconds elapsed between
+ * 1 January 1970 00:00:00 UTC and the given date
+ */
 export const getCardDate = date => new Date(date).getTime();
 
+/**
+ * Filters cards by the date range set in the card
+ * @param {Array} _cards - cards array
+ * @returns {Array} - filtered cards array:
+ * - cards with no date range set
+ * - cards with date range that contains current date
+ */
 export const filterCardsByDateRange = (_cards) => {
     const currentDate = new Date().getTime();
 
