38a2ad78a052e18a57458e640e64a5005cf8ae66
Denis Radai <yodeco@gmail.com>
2020-10-30 13:07:17 +0200

    Helpers/context.js refactored

--- react/src/js/components/Consonant/Helpers/contexts.js
+++ react/src/js/components/Consonant/Helpers/contexts.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
+/**
+ * A funtion-placeholder, does nothing
+ * @returns {undefined} - returns undefined
+ */
 export const noOp = () => {};
 
+/**
+ * Creates context for expandable components
+ * @param {Object} - default value
+ * @returns {Object} - ExpandableContext context object
+ */
 export const ExpandableContext = React.createContext({ value: null, setValue: noOp });
+
+/**
+ * Creates configuration context
+ * @param {Object} - default value
+ * @returns {Object} - ConfigContext context object
+ */
 export const ConfigContext = React.createContext({});
