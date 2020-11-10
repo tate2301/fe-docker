0f7c5e02fa17f42b6685e1de67f4500635902b3b
nanbratan <morfemi9498@gmail.com>
2020-11-10 17:25:17 +0300

    Consonant/Infobit/Type/Button/*

---
+++ react/src/js/components/Consonant/Infobit/Type/Button/constants.js
@@ -0,0 +1,13 @@
+export const BUTTON_STYLE = {
+    PRIMARY: 'primary',
+    CTA: 'call-to-action',
+};
+
+export const defaultProps = {
+    href: '',
+    text: '',
+    iconSrc: '',
+    iconAlt: '',
+    iconPos: '',
+    style: BUTTON_STYLE.CTA,
+};
