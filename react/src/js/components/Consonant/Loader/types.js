7e322499aaf1e0d915ac735dca64003c34de5355
nanbratan <morfemi9498@gmail.com>
2020-11-10 17:20:22 +0300

    Consonant/Loader/*

---
+++ react/src/js/components/Consonant/Loader/types.js
@@ -0,0 +1,11 @@
+import {
+    bool,
+    string,
+} from 'prop-types';
+
+// eslint-disable-next-line import/prefer-default-export
+export const LoaderType = {
+    size: string,
+    hidden: bool,
+    absolute: bool,
+};
