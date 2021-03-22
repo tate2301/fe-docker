ee5ac5072f0bcf5391e65540c3a358b30913d40c
yodeco <yodeco@gmail.com>
2021-03-22 22:00:19 +0200

    Apply SUIT to SearchIcon.jsx

--- react/src/js/components/Consonant/Search/SearchIcon.jsx
+++ react/src/js/components/Consonant/Search/SearchIcon.jsx
@@ -26,7 +26,7 @@ const SearchIcon = (props) => {
         <button
             data-testid="search-icon"
             type="button"
-            className="search-ico"
+            className="consonant-SearchIco"
             onClick={onClick}
             tabIndex="0">
             <span />
